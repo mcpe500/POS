77dd0a0d38e4c67c01b770b4f56d3792a5458333
mcpe500 <ivan04.android@gmail.com>
2024-01-14 14:38:51 +0700

    added login activity

--- app/src/main/java/com/example/pos/LoginActivity.kt
+++ app/src/main/java/com/example/pos/LoginActivity.kt
@@ -2,10 +2,30 @@ package com.example.pos
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val editTextEmail: EditText = findViewById(R.id.editTextTextEmailAddress)
+        val editTextPassword: EditText = findViewById(R.id.editTextTextPassword)
+        val buttonLogin: Button = findViewById(R.id.button)
+
+        buttonLogin.setOnClickListener{
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if (email == "user@gmail.com" && password == "password") {
+                // Login berhasil
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                // Login gagal
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
