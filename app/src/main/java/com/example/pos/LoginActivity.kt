79f4521562ee4bc0e45be6fd707d548e2abcb823
mcpe500 <ivan04.android@gmail.com>
2024-01-14 13:32:03 +0700

    added Login Activity xml and .kt

---
+++ app/src/main/java/com/example/pos/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pos
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
